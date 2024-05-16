21a4ba5513ddce834560e9f269190b517319fd76
ritu-477 <ritujoharbishnoi@gmail.com>
2024-05-16 12:33:56 +0530

    responsive navbar done

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflowhidden")
+    document.querySelector(".iconbox").classList.toggle("cross")
+}
