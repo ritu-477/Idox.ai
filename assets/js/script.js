a3515099b01d6a5a6355230e5fbaa9d322f6599a
ritu-477 <ritujoharbishnoi@gmail.com>
2024-05-18 11:13:06 +0530

    collaboration-section slider done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,19 @@ function openNav() {
     document.body.classList.toggle("overflowhidden")
     document.querySelector(".iconbox").classList.toggle("cross")
 }
+
+
+$('.slider_collaboration').slick({
+    arrows: false,
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true,
+    slidesToShow: 6,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 1000,
+    pauseOnHover: true,
+    cssEase: 'linear',
+});
