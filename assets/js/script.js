d94b5ccebfb8edbe6534a5a63d534267211d21e1
ritu-477 <ritujoharbishnoi@gmail.com>
2024-05-28 14:14:59 +0530

    choose the plan best for you section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,22 @@ accordionbtn.forEach((e) => {
         activeaccodion && activeaccodion.classList.remove("active")
     })
 });
+// scroll-to -top-button////
+const mybutton = document.querySelector(".topBtn");
+const show = document.querySelector(".show")
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 600) {
+        mybutton.classList.add("show")
+    }
+    else {
+        mybutton.classList.remove("show")
+    }
+})
+mybutton.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+
+});
 
 
 $('.slider_collaboration').slick({
@@ -35,7 +51,7 @@ $('.Testimonials_slider').slick({
     speed: 300,
     centerMode: true,
     centerPadding: '60px',
-    // autoplay: true,
+    autoplay: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     responsive: [
